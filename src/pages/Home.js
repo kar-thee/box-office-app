fce093a7c53c4236bb854806523485e353fcfc52
kar-thee <ourworldourpeople@gmail.com>
2021-09-16 10:45:10 +0530

    Used Axios to get info using TVMaze APi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,40 @@
-import React from "react"
-import MainPageLayout from "../components/MainPageLayout";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Home = () =>{
-    console.log("Hello")
-    return (
-        <>
-        <MainPageLayout>This is HomePage</MainPageLayout>
-        </>
-    );
-}
+import MainPageLayout from '../components/MainPageLayout';
+
+const Home = () => {
+  const [inputState, setInputState] = useState('');
+
+  const inputChange = ev => {
+    setInputState(ev.target.value);
+    console.log(ev.target.value);
+  };
+  const searchBtn = async () => {
+    const url = `https://api.tvmaze.com/search/shows?q=${inputState}`;
+    const { data } = await axios.get(url);
+    console.log(data);
+  };
+  const enterBtn = ev => {
+    if (ev.keyCode === 13) {
+      searchBtn();
+    }
+  };
+  return (
+    <>
+      <MainPageLayout>
+        <input
+          type="text"
+          onChange={inputChange}
+          value={inputState}
+          onKeyDown={enterBtn}
+        />
+        <button type="button" onClick={searchBtn}>
+          Search
+        </button>
+      </MainPageLayout>
+    </>
+  );
+};
 
 export default Home;
